02d843bb546b6ce8aeceacf6c41f01ae7b38d57c
Trevor Atkinson <tsa@structx.io>
2024-03-17 14:08:15 -0600

    marshal wallet (#6)

--- wallet.go
+++ wallet.go
@@ -3,10 +3,14 @@ package wallet
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"go.dedis.ch/kyber/v3"
 	"go.dedis.ch/kyber/v3/group/edwards25519"
+	"go.dedis.ch/kyber/v3/suites"
 	"golang.org/x/crypto/salsa20"
 )
 
@@ -17,9 +21,16 @@ var (
 
 // Wallet ...
 type Wallet struct {
-	p kyber.Scalar
-	P kyber.Point
-	s kyber.Group
+	p    kyber.Scalar
+	P    kyber.Point
+	Addr string
+	s    suites.Suite
+}
+
+type exportWallet struct {
+	PublicKey  []byte `json:"public_key"`
+	PrivateKey []byte `json:"private_key"`
+	Address    string `json:"address"`
 }
 
 // New return new wallet
@@ -32,14 +43,19 @@ func New() *Wallet {
 	X := suite.Point().Mul(x, nil) // public key
 
 	return &Wallet{
-		P: X,
-		p: x,
-		s: suite,
+		P:    X,
+		p:    x,
+		s:    suite,
+		Addr: "",
 	}
 }
 
 // Address from wallet public key
 func (w *Wallet) Address() (string, error) {
+
+	if w.Addr != "" {
+		return w.Addr, nil
+	}
 
 	publicbytes, err := w.P.MarshalBinary()
 	if err != nil {
@@ -50,4 +66,93 @@ func (w *Wallet) Address() (string, error) {
 	salsa20.XORKeyStream(out, publicbytes, nonce[:], &key)
 
 	return hex.EncodeToString(out), nil
+}
+
+// MarshalToFile export wallet to file
+func (w *Wallet) MarshalToFile(path string) error {
+
+	// transform wallet to export wallet
+	ew, err := createExportWallet(w)
+	if err != nil {
+		return fmt.Errorf("unable to create export wallet %v", err)
+	}
+
+	// convert export wallet to bytes
+	walletbytes, err := json.Marshal(ew)
+	if err != nil {
+		return fmt.Errorf("failed to marshal bytes %v", err)
+	}
+
+	// write bytes to file
+	path = filepath.Clean(path)
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("failed to create wallet file %v", err)
+	}
+	defer func() { _ = f.Close() }()
+
+	_, err = f.Write(walletbytes)
+	if err != nil {
+		return fmt.Errorf("failed to write wallet to file %v", err)
+	}
+
+	return nil
+}
+
+// UnmarshalFromFile read wallet from file
+func UnmarshalFromFile(path string) (*Wallet, error) {
+
+	path = filepath.Clean(path)
+	filebytes, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %v", err)
+	}
+
+	var export exportWallet
+	err = json.Unmarshal(filebytes, &export)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal bytes %v", err)
+	}
+
+	var w Wallet
+	w.s = edwards25519.NewBlakeSHA256Ed25519()
+
+	w.P = w.s.Point()
+	w.p = w.s.Scalar()
+
+	err = w.P.UnmarshalBinary(export.PublicKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal public key %v", err)
+	}
+
+	err = w.p.UnmarshalBinary(export.PublicKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal private key %v", err)
+	}
+
+	return &w, nil
+}
+
+func createExportWallet(w *Wallet) (*exportWallet, error) {
+
+	publicbytes, err := w.P.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal public key %v", err)
+	}
+
+	privatebytes, err := w.p.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal private key %v", err)
+	}
+
+	addr, err := w.Address()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read wallet address %v", err)
+	}
+
+	return &exportWallet{
+		PublicKey:  publicbytes,
+		PrivateKey: privatebytes,
+		Address:    addr,
+	}, nil
 }
--- wallet_test.go
+++ wallet_test.go
@@ -1,1 +1,33 @@
 package wallet_test
+
+import (
+	"testing"
+
+	"github.com/trevatk/go-wallet"
+)
+
+func Test_Marshal(t *testing.T) {
+	t.Run("to_file", func(t *testing.T) {
+		w := wallet.New()
+		err := w.MarshalToFile("./testfiles/w1.json")
+		if err != nil {
+			t.Fatalf("failed to marshal wallet to file %v", err)
+		}
+	})
+}
+
+func Test_UnmarshalFromFile(t *testing.T) {
+	t.Run("from_file", func(t *testing.T) {
+		w, err := wallet.UnmarshalFromFile("./testfiles/w1.json")
+		if err != nil {
+			t.Fatalf("failed to unmarshal wallet %v", err)
+		}
+
+		addr, err := w.Address()
+		if err != nil {
+			t.Fatalf("failed to generate wallet address %v", err)
+		}
+
+		t.Logf("wallet address %s", addr)
+	})
+}
